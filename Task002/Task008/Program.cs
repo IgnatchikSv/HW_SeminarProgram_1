a0c5524572415481095f8c196439bb4d994934fd
Svetlana Ignatchik <lightbug89@gmail.com>
2022-12-22 19:30:13 +0300

    Task008

---
+++ Task002/Task008/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear ();
+
+Console.Write ("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int b = 2;
+
+if ( numberA > 1)
+{
+  while (b <= numberA)
+  { Console.Write ( b + " ");
+  b = b + 2;
+  }
+}
